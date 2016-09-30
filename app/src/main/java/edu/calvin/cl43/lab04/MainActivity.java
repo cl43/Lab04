6d89fc15ee957cb5d58c67b1ec48ce650932c8a6
Chris Li <cl43@calvin.edu>
2016-09-30 17:04:33 -0400

    Initial commit

---
+++ app/src/main/java/edu/calvin/cl43/lab04/MainActivity.java
@@ -0,0 +1,59 @@
+package edu.calvin.cl43.lab04;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ *  Chris Li, cl43, cs262, lab 04, Prof. Vanderlinden
+ *  <p>
+ *  This app has a menu in the top-right corner that takes you to an about page. User can use the Android return button to go back to the main page
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        /* Although it was the hero's INTENT to slay the evil beast Dividius Zero.
+         * He put on his armor and equipped his weapon, but much was on his mind.
+         * The hero knew that this was his destiny to free Aerith Maticia and save its people.
+         * But he wondered "Why me? Why was I chosen?".
+         * IF he did not find out why he was the chosen hero, he could not put himself in the action's setting.
+         * He traveled on to other realms to learn what the beast, Dividius Zero, is really ABOUT.
+         */
+        if (id == R.id.action_settings) {
+            //If the about page has been clicked, it will bring it up.
+            Intent intent=new Intent(this,Main2Activity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
